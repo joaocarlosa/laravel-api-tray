3c0aa6efb7401656c80dc71f11703f3d07b107d6
joaocarlosa <joaocarloslima99@gmail.com>
2023-09-17 20:44:45 -0300

    feat: implementa seller Resource e request

---
+++ app/Http/Requests/SellerRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SellerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:sellers,email',
+        ];
+    }
+}
