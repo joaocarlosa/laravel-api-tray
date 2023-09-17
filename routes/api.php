52478a3a48e505d0e1dcd9183a29c8cdc2ee5c2b
joaocarlosa <joaocarloslima99@gmail.com>
2023-09-17 18:56:32 -0300

    chore: altera configuraçoes basicas do laravel

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/', function(){
+    return response()->json([
+        'success' =>  true
+    ]);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
