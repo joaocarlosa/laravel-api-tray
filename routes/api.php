a3f80eedba4ea536e378b53d01f3f014c953a271
joaocarlosa <joaocarloslima99@gmail.com>
2023-09-17 20:43:56 -0300

    feat: implementa APiResouce

--- routes/api.php
+++ routes/api.php
@@ -2,22 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SaleController;
+use App\Http\Controllers\SellerController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
+
 Route::get('/', function(){
     return response()->json([
         'success' =>  true
     ]);
 });
+
+Route::apiResource('sellers', SellerController::class);
+Route::apiResource('sales', SaleController::class);
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
