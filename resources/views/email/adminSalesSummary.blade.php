cfea64591b68ba953c8cb154af41f27620e7f5f3
joaocarlosa <joaocarloslima99@gmail.com>
2023-09-18 14:08:19 -0300

    feat: cria templates de email

---
+++ resources/views/email/adminSalesSummary.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Resumo de vendas</title>
+</head>
+<body>
+    <h1>Resumo de Vendas do Dia</h1>
+    <p>Total de vendas: {{ $totalSales }}</p>
+</body>
+</html>
---
+++ resources/views/email/sellerSalesSummary.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Resumo de vendas</title>
+</head>
+<body>
+    <h1>Resumo de vendas para hoje</h1>
+    <p>Total de vendas: {{ $totalSales }}</p>
+    <p>Comissão total: {{ $totalCommission }}</p>
+</body>
+</html>
